0f4011a240e4ed87f48ac60ac96d44b1a62f73dc
Natcha Klarjing <64050071@KMITL.AC.TH>
2024-03-15 02:17:10 +0700

    update

--- Project/server/server.js
+++ Project/server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-app.use(express.static('public')); // เปิดการใช้งานไฟล์ส่วนหน้าในโฟลเดอร์ public
+app.use(express.static(path.join(__dirname, 'public'))); // เปิดการใช้งานไฟล์ส่วนหน้าในโฟลเดอร์ public
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'Project/public/index.html'); // ส่งไฟล์ HTML หลัก
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // ส่งไฟล์ HTML หลัก
 });
 
 app.listen(port, () => {
-  console.log(`เซิร์ฟเวอร์กำลังทำงานที่ http://Adivino:${port}`);
+  console.log(`เซิร์ฟเวอร์กำลังทำงานที่ :${port}`);
 });
